ceb82d6def8f7cf58fd100f31410136a40fbaa07
JamesBear <jamesgrizzly@gmail.com>
2016-07-25 14:08:38 +0800

    init commit

---
+++ Assets/Scripts/ScanQRCode.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using ZXing;
+
+public class ScanQRCode : MonoBehaviour {
+
+    private const int CHECK_FREQUENCY = 5;
+
+    ARController arCtl;
+    
+
+    public bool btnTest;
+    public TextMesh text;
+    public GameObject attachPoint;
+
+    private int frameConuter;
+
+    void Awake()
+    {
+        arCtl = GetComponent<ARController>();
+        frameConuter = 0;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    bool OnButton(ref bool btnIndicator)
+    {
+        if (btnIndicator)
+        {
+            btnIndicator = false;
+            return true;
+        }
+
+        return false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        frameConuter++;
+	    //if (OnButton(ref btnTest))
+        if (frameConuter % CHECK_FREQUENCY == 0)
+        {
+            Test();
+        }
+
+        GameObject videoSource = GameObject.Find("Video source 0");
+        if (videoSource != null && videoSource.transform.parent == null)
+        {
+            videoSource.transform.parent = attachPoint.transform;
+        }
+
+    }
+
+    void Test()
+    {
+        //Debug.Log(arCtl._videoColor32Array0);
+        //Debug.Log(arCtl._videoColorArray0);
+        //Debug.Log(arCtl._videoTexture0);
+        if (arCtl._videoTexture0 == null)
+            return;
+
+        
+        // create a barcode reader instance
+        IBarcodeReader reader = new BarcodeReader();
+        // load a bitmap
+        var barcodeBitmap = arCtl._videoTexture0.GetRawTextureData();// (Bitmap)Bitmap.LoadFrom("C:\\sample-barcode-image.png");
+        // detect and decode the barcode inside the bitmap
+        var result = reader.Decode(barcodeBitmap, arCtl._videoTexture0.width, arCtl._videoTexture0.height, RGBLuminanceSource.BitmapFormat.ARGB32);
+        // do something with the result
+        if (result != null)
+        {
+            string txtDecoderType = result.BarcodeFormat.ToString();
+            string txtDecoderContent = result.Text;
+
+            Debug.Log("txtDecoderType = " + txtDecoderType);
+            Debug.Log("txtDecoderContent = " + txtDecoderContent);
+            text.text = txtDecoderContent;
+        }
+        else
+        {
+            //Debug.Log("QRScan: null result");
+        }
+
+    }
+}
---
+++ README.txt
@@ -0,0 +1,11 @@
+About this program
+--------------------
+This is a QR code scanner project using HTC Vive's front camera. To use it, you need to put on a HTC Vive and put the phone close to the HMD front camera so that you can see the QR code on your phone very clearly in the HMD. The decoded QR code will be shown in front of you.
+
+Author
+--------------------
+james_xiong@htc.com
+
+
+
+Copyrighted to HTC Corporation.
